f42a6952844523dd6ca22efe76f98a3642011b6e
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-06-02 21:53:20 +0300

    src/errors/handleErrors.ts: update

--- src/errors/handleErrors.ts
+++ src/errors/handleErrors.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import { ServerError } from './ServerError';
 
 export default (err: ServerError, req: Request, res: Response, next: Function) => {
+  const message = err.statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res
     .status(err.statusCode)
-    .send({ message: err.message });
+    .send({ message });
   next();
 };
